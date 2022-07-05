6889bab6edb597fad3a53e3a895d2326e09271c2
ardaoz123 <60921183+ardaoz123@users.noreply.github.com>
2022-07-05 02:24:22 +0200

    1-8

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
+
+export default function App() {
+    return (
+        <div>
+            <h1>Bookkeeper!</h1>
+            <nav
+                style={{
+                    borderBottom: "solid 1px",
+                    paddingBottom: "1rem",
+                }}
+            >
+                <Link to="/AlbumOverview">Album overview</Link> |{" "}
+            </nav>
+        </div>
+    );
+}
---
+++ src/components/AlbumOverview.js
@@ -0,0 +1,7 @@
+export default function AlbumOverview() {
+    return (
+        <main style={{ padding: "1rem 0" }}>
+            <h2>Album Overview!!!</h2>
+        </main>
+    );
+}
